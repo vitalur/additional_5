45c479d6416339dd6bf5dea0892ffd1ade34358d
vitalur <39513998+vitalur@users.noreply.github.com>
2018-09-22 21:36:17 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  	var narray = bracketsConfig + '';
+  	narray = narray.replace(/[,]/g, '');
+
+  	for (let i = 0; i < str.length; i++) {
+  		for (let j = 0; j < narray.length; j+=2) {
+  			if (str[i] + str[i + 1] === narray[j] + narray[j + 1]) {
+  				str = str.replace(str[i] + str[i + 1], '');
+  				i = 0;
+  			}
+  		}
+  	}
+  	for (let j = 0; j < narray.length; j+=2) {
+  		if (str === narray[j] + narray[j + 1]) {
+  			str = str.replace(str, '');
+  		}
+  	}
+
+  	return str === '';
+ }
